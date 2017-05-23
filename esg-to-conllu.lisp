59f845c15940b90af158d95474c924bc0cb8abc2
João Marcos Gris <joaomarcosgris@hotmail.com>
2017-05-23 13:57:36 -0300

    second attempt to convert esg to conllu

--- esg-to-conllu.lisp
+++ esg-to-conllu.lisp
@@ -1,19 +1,26 @@
 (ql:quickload "cl-conllu")
 (ql:quickload "xmls")
 (ql:quickload "cl-ppcre")
+
 (in-package :cl-conllu)
 
 ;; ESGSentence -> CoNLL-USentence
 ;; produce a approximate CoNLL-U sentence from a ESG one
 
-(defun read-esg (esg-filename)
-  (cxml:parse-file esg-filename (cxml-xmls:make-xmls-builder)))
+(defun file-string (path)
+  (with-open-file (stream path)
+    (let ((data (make-string (file-length stream))))
+      (read-sequence data stream)
+      data)))
 
-(defun get-attribute (attr esg-document)
-  (car (cdr (member attr (apply #'append (cxml-xmls:node-attrs esg-document)) :test #'equal))))
+(defun read-esg (esg-filename)
+  (xmls:parse (file-string esg-filename)))
+
+(defun get-attribute (attr xml)
+  (xmls:xmlrep-attrib-value attr xml))
 
 (defun node-name-equal? (xml name)
-  (equal name (cxml-xmls:node-name xml)))
+  (equal name (xmls:xmlrep-tag xml)))
 
 (defun set-text (sentence xml)
   (setf (sentence-meta sentence)
@@ -31,39 +38,36 @@
 	  tokens)))
 
 (defun modify-token-from-hd (xml token id)
-  (let* ((form (get-attribute "w" xml))
-	 (lemma (get-attribute "c" xml))
-	 (s (get-attribute "s" xml))
-	 (a (get-attribute "a" xml))
+  (let* ((form (xmls:xmlrep-attrib-value "w" xml))
+	 (lemma (xmls:xmlrep-attrib-value "c" xml))
+	 (s (xmls:xmlrep-attrib-value "s" xml))
+	 (a (xmls:xmlrep-attrib-value "a" xml))
 	 (deps (concatenate 'string "s:" (write-to-string s) " " "a:" (write-to-string a))))
     (setf (token-id token) id)
     (setf (token-form token) form)
     (setf (token-lemma token) lemma)
     (setf (token-deps token) deps)))
 
+(defun process-token (sentence xml tokens token-id)
+  (cond ((node-name-equal? xml "ph") (setf tokens (build-token-from-ph tokens xml)))
+	((node-name-equal? xml "hd") (progn
+				       (modify-token-from-hd xml (car tokens) token-id)
+				       (insert-token sentence (car tokens))
+				       (incf token-id)
+				       (setf tokens (cdr tokens)))))
+  (print tokens)
+  (values token-id tokens))
+
+(defun process-esg (sentence xml tokens token-id)
+  (multiple-value-bind (token-id tokens) (process-token sentence xml tokens token-id))
+  (if (car (xmls:xmlrep-children xml)) (process-esg sentence (car (xmls:xmlrep-children xml)) tokens token-id))
+  (if (cdr (xmls:xmlrep-children xml)) (process-esg sentence (cdr (xmls:xmlrep-children xml)) tokens token-id))
+  sentence)
+
 (defun esg-to-conllu (esg-document)
   (let ((sentence (make-instance 'sentence))
 	(tokens nil)
-	(token-id 1))
-    (do ((xml esg-document (xmls:xmlrep-children xml)))
-	((equal xml nil) sentence)
-      (setf tokens (get-tokens (car xml) token-id))
-      (setf tokens (get-tokens (cdr xml)))
-	
-      (cond ((node-name-equal? xml "seg") (set-text sentence xml))
-	    ((node-name-equal? xml "ph") (setf tokens (build-token-from-ph tokens xml)))
-	    ((node-name-equal? xml "hd") (progn
-					   (modify-token-from-hd xml (car tokens) token-id)
-					   (insert-token sentence (car tokens))
-					   (incf token-id)
-					   (setf tokens (cdr tokens))))))))
-
-
-
-
-
-
-
-
-
-
+	(token-id 1)
+	(xml esg-document))
+    (set-text sentence xml)
+    (process-esg sentence (car (xmls:xmlrep-children xml)) tokens token-id)))
