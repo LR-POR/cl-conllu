8e0a9584d3ab9b94b054c605366dd1ad04a8c5aa
Fabricio Rosario <fchalub@br.ibm.com>
2017-05-26 12:27:36 -0300

    reorder tokens; fixed extra character in packages.lisp

--- esg.lisp
+++ esg.lisp
@@ -1,6 +1,6 @@
 (in-package :cl-conllu)
 
-(defun g (key list)
+(defun avalue (key list)
   (cadr (assoc key list :test #'equal)))
 
 (defun num-token-id (tk)
@@ -9,27 +9,17 @@
 (defun replace-spaces (str)
   (substitute #\_ #\space str))
 
-(defconstant +sentlenmax+ 100 "The maximum number of tokens in a sentence.  Needs to be the same value as in esg.")
-
-(defun reorder-list (list i j)
-  "Move J after I in list LIST.  Example:
-   (reorder-list '(1 2 3 4 5 103 104) 2 4) => '(1 2 3 103 4 104)"
-  (let ((tmp (cons (nth j list) (nthcdr (1+ i) list))))
-    (rplacd (nthcdr (1- j) list) (nthcdr (1+ j) list))
-    (rplacd (nthcdr i list) tmp)
-    list))
-
 (defun ph (ph head sentence)
   (let* ((metadata (cadr ph))
-         (features (g "f" metadata))
+         (features (avalue "f" metadata))
          (split-features (split-sequence:split-sequence #\space features))
-         (slot (g "slot" metadata))
-         (id (g "id" metadata))
-         (hd (g "hd" (cddr ph)))
-         (word (replace-spaces (g "w" hd)))
-         (citation-form (replace-spaces (g "c" hd)))
-         (sense (replace-spaces (g "s" hd)))
-         (frame (g "a" hd)))
+         (slot (avalue "slot" metadata))
+         (id (avalue "id" metadata))
+         (hd (avalue "hd" (cddr ph)))
+         (word (replace-spaces (avalue "w" hd)))
+         (citation-form (replace-spaces (avalue "c" hd)))
+         (sense (replace-spaces (avalue "s" hd)))
+         (frame (avalue "a" hd)))
 
     (dolist (c (cdr ph))
       (when (equal "ph" (car c)) (ph c id sentence))
@@ -48,9 +38,9 @@
         (metadata (cadr seg))
         (top (caddr seg)))
     (ph top 0 sentence)
-    (setf (sentence-meta sentence) (pairlis '("text") `(,(g "text" metadata))))
+    (setf (sentence-meta sentence) (pairlis '("text") `(,(avalue "text" metadata))))
     (setf (sentence-tokens sentence) (reverse (sentence-tokens sentence)))
-    sentence))
+    (adjust-sentence sentence)))
 
 (defun read-esg (infile)
   (with-open-file (in infile)
