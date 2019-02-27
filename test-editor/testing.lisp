283a78ca4bf5fd5ea1191d830073f44227a5b7e2
William Sena <will182neves@gmail.com>
2019-02-27 17:43:21 -0300

    add testes

---
+++ test-editor/testing.lisp
@@ -0,0 +1,51 @@
+(ql:quickload :cl-conllu)
+
+(let ((rule-1 
+       '((rule
+	  :vars (((= upostag "PROPN") (= deprel "flat:name"))
+		 ((= upostag "PROPN") (= deprel "nmod"))
+		 ((= upostag "NOUN") (= deprel "nsubj"))
+		 (= upostag "PRON"))
+	  :rels ((> 4 3 2 1))
+	  :acts ((> 3 1)
+		 (> 2 3)
+		 (> 4 2)
+		 (set 1 deprel "CASE")
+		 (set 2 deprel "ADVERB")
+		 (set 3 deprel "DET")
+		 (set 4 deprel #'string-upcase)))))
+      (rule-2
+       '((rule
+	  :vars ((~ id .*)
+		 (not (= form "nothing"))
+		 ((~ lemma ".*") (not (= form "nothing")))
+		 ((~ head .*) (~ id .*) (~ form ".*"))
+		 (not (= upostag ".*")))
+	  :rels ((> 1 2 3 4 5))
+	  :acts ((> 5 4 3 2 1) (last)))
+	 (rule
+	  :vars ((~ id .*)
+		 (~ form ".*"))
+	  :rels ((> 1 2)
+		 (distance 1 2 -100 100)
+		 (not (= 1 2 form upostag)))
+	  :acts ((> 2 1)
+		 (set 1 form "fuck")
+		 (set 2 1 upostag form)
+		 (set 2 form #'string-upcase))))))
+  (format t "~a ~t" (if (tree-equal
+			 (with-open-file (str "test.txt")
+			   (read str))
+			 (list (conllu.editor:conlluedit (cl-conllu:read-conllu #P"test.conllu") rule-1)
+			       (conllu.editor:conlluedit (cl-conllu:read-conllu #P"test.conllu") rule-2))
+			 :test #'super-equal)
+			"Teste realizado com sucesso! :) "
+			"Teste falhou! :( ")))
+
+(defun super-equal (a b)
+  (cond ((integerp a)
+	 (= a b))
+	((stringp a)
+	 (string= a b))
+	(t
+	 (eql a b))))
