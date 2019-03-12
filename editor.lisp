8c1ab607cf4cd827a80622a7e012f2c9b49d5ad3
William Sena <will182neves@gmail.com>
2019-03-12 17:06:59 -0300

    adicionando funcionalidades do rules.lisp

--- editor.lisp
+++ editor.lisp
@@ -1,6 +1,7 @@
 
 (in-package :conllu.editor)
 
+
 (defun conlluedit (sentences rules)
   (reduce #'(lambda (acumulated-results sentence)
 	      (let* ((sent-id (sentence-id sentence)) 
@@ -45,7 +46,6 @@
 			    rule-index (length results) sent-id)
 		    (cons (first result-actions) results)))))))))))
 
-
 ;; part 1
 
 (defun definitions (definitions &optional (index 1) result-defs)
@@ -58,7 +58,8 @@
 						    (list first-def))))
 		result-defs)))
       result-defs))
-  
+
+
 (defun def-match (def)
   (let* ((negative (string= (first def) 'not))
 	 (expression (if negative (nth 1 def) def))
@@ -67,22 +68,28 @@
     (set-match-test (if (or (string= criterion 'id) (string= criterion 'head))
 			(write-to-string value)
 			value)
-		    criterion negative (string= (first expression) '~))))
-    
-(defun set-match-test (value criterion negative-criterion regular-expression)
+		    criterion negative (first expression))))
+
+
+(defun set-match-test (value criterion negative-criterion operation)
   #'(lambda (token)
       (let ((field (get-field-value criterion token)))
 	(when field
 	  (funcall (if negative-criterion #'not #'identity)
-		   (if regular-expression
-		       (cl-ppcre:scan value field)
-		       (string= value field)))))))
+		   (cond ((string= operation '~)
+			  (cl-ppcre:scan value field))
+			 ((string= operation '%)
+			  (find value (cl-ppcre:split "\\|" field) :test #'string=))
+			 (t
+			  (string= value field))))))))
+
 
 (defun relations (relations)
   (mapcar #'relation
 	  (reduce #'(lambda (result relation)
 		      (append (normalize-shorthand relation '=) result))
 		  relations :initial-value nil)))
+
 
 (defun relation (relation)
   (let* ((negative (string= (first relation) 'not))
@@ -91,6 +98,7 @@
      :defs (cons (nth 1 expression) (nth 2 expression))
      :relation-test (set-relation-test negative (first expression)
 				       (nth 3 expression) (nth 4 expression)))))
+
 
 (defun set-relation-test (negative first-expression field-1 field-2)
   #'(lambda (token-1 token-2)
@@ -105,11 +113,13 @@
 			    (<= null-field-1 (- id-2 id-1)
 				(if field-2 field-2 null-field-1)))))))))
 
+
 (defun actions (actions)
   (reverse (mapcar #'action
 		   (reduce #'(lambda (result relation)
 			       (append (normalize-shorthand relation 'set) result))
 			   actions :initial-value nil))))
+
 
 (defun action (expression)
   (let ((op (first expression))) 
@@ -134,7 +144,6 @@
 				     (nth 1 result))))
 		     (cddr expression) :initial-value (list (nth 1 expression) nil)))
       (list expression)))
-
 
 ;; part 2
 
@@ -163,7 +172,6 @@
 	(all-defs-test token (rest defs)))
       t))
 
-
 ;;part 3
 
 (defun sets (nodes relations &optional (relation-index 1) result-sets)
@@ -178,13 +186,15 @@
 			    :matchers (list (cons 1 (token-id matcher-1)))))
 		  (rest (assoc 1 nodes))))))
 
+
 (defun relation-match (nodes relation relation-index)
   (let* ((defs (getf relation :defs))
 	 (def-1 (first defs))
 	 (def-2 (rest defs)))
     (result-sets (getf relation :relation-test) relation-index def-1 def-2
 		 (rest (assoc def-1 nodes)) (rest (assoc def-2 nodes)))))
- 							    
+
+
 (defun result-sets (relation-test relation-index def-1 def-2 nodes-1 nodes-2)
   (reduce #'(lambda (result-1 first-1)
 	      (reduce #'(lambda (result-2 first-2)
@@ -193,13 +203,13 @@
 			    (if set (cons set result-2) result-2)))
 		      nodes-2 :initial-value result-1))
 	  nodes-1 :initial-value nil))
-  
+
+
 (defun result-set (token-1 token-2 def-1 def-2 relation-test relation-index)
   (if (funcall relation-test token-1 token-2)
       (list :relation-index (list relation-index)
 	    :matchers (list (cons def-1 (token-id token-1))
 			    (cons def-2 (token-id token-2))))))
-
 
 ;; part 4  
 
@@ -212,6 +222,7 @@
 				(append (list set) merged-matchers result-sets)))
 			  sets :initial-value nil)))
 
+
 (defun multiple-merges (defs-count rels-count bin-1 bins)
   (reduce #'(lambda (result-matchers bin)
 	      (let ((merged-matchers (merge-matchers defs-count rels-count bin-1 bin)))
@@ -219,7 +230,8 @@
 		    (cons merged-matchers result-matchers)
 		    result-matchers)))
 	  bins :initial-value nil))
-		
+
+
 (defun merge-matchers (defs-count rels-count bin-1 bin-2)
   (let ((matchers-union (union (getf bin-1 :matchers) (getf bin-2 :matchers)
 			       :test #'tree-equal))
@@ -230,6 +242,7 @@
       (list :relation-index relation-indexes
 	    :matchers matchers-union))))
 
+
 (defun union-matchers (defs-count rels-count sets)
   (reduce #'(lambda (result-sets set)
 	      (let ((matcher (getf set :matchers)))
@@ -239,7 +252,6 @@
 		    result-sets)))
 	  sets :initial-value nil))
   
-
 ;; part 5
 
 (defun result-actions (sets tokens actions &optional result-actions)
@@ -256,10 +268,8 @@
 (defun result-action (sets tokens action)
   (let* ((field (nth 3 action))
 	 (result-assocs (assocs (nth 2 action) sets field tokens)) 
-	 (result-ids (first result-assocs))
-	 (result-tokens (nth 1 result-assocs)) 
-	 (old-fields (nth 2 result-assocs)))
-    (list field result-tokens result-ids old-fields
+	 (result-tokens (nth 1 result-assocs)))
+    (list field result-tokens (first result-assocs) (nth 2 result-assocs)
 	  (mapcar #'(lambda (token)
 		      (let ((value 
 			     (cond ((nth 1 action)
@@ -273,6 +283,19 @@
 			    value
 			    (add-or-subt (first action) field token value))))
 		  result-tokens))))
+
+
+(defun assocs (value sets field tokens &optional ids result-tokens old-fields)
+  (if sets
+      (let ((set (first sets)))
+	(if (= value (first set))
+	    (let* ((id (rest set))
+		   (token (nth (1- id) tokens))
+		   (old-field (get-field-value field token)))
+	      (assocs value (rest sets) field tokens (cons id ids)
+		      (cons token result-tokens) (cons old-field old-fields)))
+	    (assocs value (rest sets) field tokens ids result-tokens old-fields)))
+      (list ids result-tokens old-fields)))
 
 
 (defun add-or-subt (op field token value)
@@ -293,30 +316,17 @@
 (defun test-feats-misc (string-1 string-2)
   (string= (first (cl-ppcre:split "=" string-1))
 	   (first (cl-ppcre:split "=" string-2))))
-    
+  
 
 (defun clear (string)
   (let ((length-string (length string)))
-    (cond ((< length-string 1)
+    (cond ((< length-string 2)
 	   "_")
 	  ((or (char= (elt string 0) #\|) (char= (elt string 0) #\_))
 	   (clear (subseq string 1)))
 	  ((or (char= (elt string (1- length-string)) #\|) (char= (elt string (1- length-string)) #\_))
-	   (clear (subseq string 0 (- length-string 2))))
+	   (clear (subseq string 0 (- length-string 1))))
 	  (t (cl-ppcre:regex-replace-all "\\|\\|" string "|")))))
-
-
-(defun assocs (value sets field tokens &optional ids result-tokens old-fields)
-  (if sets
-      (let ((set (first sets)))
-	(if (= value (first set))
-	    (let* ((id (rest set))
-		   (token (nth (1- id) tokens))
-		   (old-field (get-field-value field token)))
-	      (assocs value (rest sets) field tokens (cons id ids)
-		      (cons token result-tokens) (cons old-field old-fields)))
-	    (assocs value (rest sets) field tokens ids result-tokens old-fields)))
-      (list ids result-tokens old-fields)))
 
 ;; part 6	      
 
@@ -329,6 +339,7 @@
 		   (not (string= 'nil (first (nth 4 result-action)))))
 	  (examine-actions (rest final-actions) t)))
       not-first))
+
 
 (defun apply-action (result-action)
   (let ((field (first result-action)))
